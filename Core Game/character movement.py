532ac7e7446b56f7d7d04590b1e3cbecfcfc2bac
ProjectCALC <projectcalcteam@gmail.com>
2017-04-21 20:02:55 -0400

    Add files via upload

---
+++ Core Game/character movement.py
@@ -0,0 +1,45 @@
+import pygame
+import sys
+import random
+         
+wizard_right = pygame.image.load('images\wizardright.png')
+wizard_left = pygame.image.load('images\wizardleft.png')
+wizard_right_sword = pygame.image.load('images\wizardswordright.png')
+wizard_left_sword = pygame.image.load('images\wizardswordleft.png')
+pygame.init()
+screen = pygame.display.set_mode((400, 300))
+pygame.display.set_caption('CalcRPG')
+done = False
+x = 30
+y = 30
+clock = pygame.time.Clock()
+
+while not done:
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    done = True
+        screen.fill((255,255,255))
+        img = pygame.image.load('images\wizardright.png')
+        sword_img = pygame.image.load('images\wizardswordleft.png')
+        grass = pygame.image.load('images\grass.jpg')
+        screen.blit(grass, (0,0,20,20))         
+        screen.blit(img, (x, y, 20, 20))
+
+        pressed = pygame.key.get_pressed()
+        
+        if pressed[pygame.K_w]:
+            y -= 3
+        if pressed[pygame.K_s]:
+            y += 3
+        if pressed[pygame.K_a]:
+            x -= 3
+            screen.fill([255,255,255])
+            screen.blit(wizard_left, (x, y, 20, 20))
+        if pressed[pygame.K_d]:
+            x += 3
+            screen.fill([255,255,255])
+            screen.blit(wizard_right, (x, y, 20, 20))
+
+
+        pygame.display.flip()
+        clock.tick(60)
