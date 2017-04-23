3ba1e2c2ae7b8952e453bfb2ee7bade8067c88d1
ProjectCALC <projectcalcteam@gmail.com>
2017-04-22 22:07:24 -0400

    Delete Random birthdate module(WIP).py

--- Modules/Random birthdate module(WIP).py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-import random
- 
-year = random.choice(range(1950, 2001))
-month = random.choice(range(1, 13))
-day = random.choice(range(1, 29))
-birth_date = datetime(year, month, day)
