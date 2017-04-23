3ef100e0dce525912585fa098fd0e2d922d0f0a0
ProjectCALC <projectcalcteam@gmail.com>
2017-04-22 21:04:35 -0400

    Update menu.py

--- Core Game/menu.py
+++ Core Game/menu.py
@@ -33,9 +33,9 @@ def game():
         cc.load()
         time.sleep(1)
         load_continue = input('Continue?(Yes or No) ')
-        if load_continue == 'No' or 'no' or 'n':
+        if load_continue == 'No' or load_continue == 'no' or load_continue == 'n':
             game()
-        elif load_continue == 'Yes' or 'yes' or 'y':
+        elif load_continue == 'Yes' load_continue == or 'yes' or load_continue == 'y':
             character_movement.main()
             sys.exit()        
     else:
