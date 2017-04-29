2a273611d0572b9f13361585d8ed5a868ed8e9ec
Noah <noah.hourdebaigt@gmail.com>
2017-04-29 14:27:05 -0400

    Update weapons.py

--- Modules/weapons.py
+++ Modules/weapons.py
@@ -5,6 +5,7 @@ def main():
 
 
     weapon_names = ['Sword','Super Sword','Hyper Sword','Mega Blade','Bow','Super Bow','Hyper Bow','Mega Bow']
+    pickle.dump( weapon_names, open( "weapons.txt", "wb" ) )
 
     sword = 5 + 0,5 * level_module.level
     super_sword = 5 + 1 * level_module.level
