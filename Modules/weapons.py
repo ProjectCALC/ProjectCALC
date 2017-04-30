1b3411a0e3bfe54135bda7bda605cb839c151996
Noah <noah.hourdebaigt@gmail.com>
2017-04-30 11:45:57 -0400

    Add files via upload

--- Modules/weapons.py
+++ Modules/weapons.py
@@ -3,20 +3,20 @@ import pickle
 import random
 
 weapon_names = ['Sword','Super Sword','Hyper Sword','Mega Blade','Bow','Super Bow','Hyper Bow','Mega Bow']
-pickle.dump( weapon_names, open( "weapons.txt", "wb" ) )
-def main():
-    sword = 5 + 0,5 * level_module.level
-    super_sword = 5 + 1 * level_module.level
-    hyper_sword = 5 + 1.5 * level_module.level
-    mega_blade = 5 + 2 * level_module.level
-    
-    bow_random = random.randint(5,8)
-    
-    bow = bow_random + 0,5 * level_module.level
-    super_bow = bow_random + 1 * level_module.level
-    hyper_bow = bow_random + 1,5 * level_module.level
-    mega_bow = bow_random + 2 * level_module.level
-    
+weapon_names_lower = ['sword','super sword','hyper sword','mega blade','bow','super bow','hyper bow','mega bow']
+pickle.dump(weapon_names, open("weapons.txt", "wb"))
+pickle.dump(weapon_names_lower, open("weapons_lower.txt", "wb"))
+
+
+sword = 5 + 0.5 * level_module.level
+super_sword = 5 + 1 * level_module.level
+hyper_sword = 5 + 1.5 * level_module.level
+mega_blade = 5 + 2 * level_module.level
+bow_random = random.randint(5,8) * level_module.level
+bow = random.randint(5,8) + 0.5 * level_module.level
+super_bow = random.randint(5,8) + 1 * level_module.level
+hyper_bow = random.randint(5,8) + 1,5 * level_module.level
+mega_bow = random.randint(5,8) + 2 * level_module.level
     
     
     
