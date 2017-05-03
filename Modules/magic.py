6f46c6b1a0c274cc6af681674e3332ed6106821a
Noah <noah.hourdebaigt@gmail.com>
2017-05-03 16:31:04 -0400

    Create magic.py

---
+++ Modules/magic.py
@@ -0,0 +1,34 @@
+import random
+import level_module
+
+#list of spells that you can cast
+magic_list = ['Fireball','Heal','Tornado','Teleport']
+
+#adds a spell to magic_list for every magic level-up
+if magic_level == 2:
+  magic_list.append('Summon')
+elif magic_level == 3:  
+  magic_list.append('Light Sword')
+elif magic_level == 4:  
+  magic_list.append('Light Bow')
+elif magic_level == 5:   
+  magic_list.append('Dark Sword')
+elif magic_level == 6:
+  magic_list.append('Dark Bow')
+elif magic_level == 7:
+  magic_list.append('Fairy Call')
+elif magic_level == 8:
+  magic_list.append('Demon Call')
+elif magic_level == 9:
+  magic_list.append('Ultra Black Hole')
+
+
+#damage
+fireball = 10(magic_level) * random.uniform(1,1.4)
+tornado = 10(magic_level) * random.uniform(1,1.4)
+light_sword = 10(magic_level) * random.uniform(1,1.4)
+light_bow = 10(magic_level) * random.uniform(1,1.4)
+dark_sword = 10(magic_level) * random.uniform(1,1.4)
+dark_bow = 10(magic_level) * random.uniform(1,1.4)
+ultra_black_hole = 10(magic_level) * random.uniform(1,1.4)
+  
