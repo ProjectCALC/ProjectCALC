340c12b6b5d36f49c0d8393fdc57418e3d879152
Noah <noah.hourdebaigt@gmail.com>
2017-05-03 16:39:41 -0400

    Update level_module.py

--- Modules/level_module.py
+++ Modules/level_module.py
@@ -1,15 +1,32 @@
 import random
 global level
-level = 1
 global xp
+global old_level
+global skill_t
+global magic_level
+level = 1
 xp = 0
+old_level = 0
+skill_t = 0
+magic_level = 0
 
 def main():
     while xp != 100:
-        break
+        pass
     while xp == 100 or xp > 100:
-        level +=1
-        print('Congrats! You''ve leveled up to level '+level+'!')
+        if level > 91:
+            pass
+        elif level = old_level + 9
+            level +=1
+            print('Congrats! You''ve leveled up to level '+level+'!')
+            magic_level +=1
+            skill_t +=1
+            old_level +=9
+            print('Good job! Your magic level is now '+magic_level+'!')
+        while level != old_level + 9:
+            pass
+        
+            
 
 def add_xp(xp):
     xp_add = random.randint(1,20)
