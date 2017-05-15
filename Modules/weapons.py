c15a20f643bb3160438bedbd74adcc24fbe85b76
Noah <noah.hourdebaigt@gmail.com>
2017-05-15 15:25:28 -0400

    Update weapons.py

--- Modules/weapons.py
+++ Modules/weapons.py
@@ -13,10 +13,10 @@ super_sword = 5 + 1 * level_module.level
 hyper_sword = 5 + 1.5 * level_module.level
 mega_blade = 5 + 2 * level_module.level
 bow_random = 5 * level_module.level
-bow = random.randint(5,6) + 0.5 * level_module.level
-super_bow = random.randint(6,7) + 1 * level_module.level
-hyper_bow = random.randint(7,10) + 1,5 * level_module.level
-mega_bow = random.randint(10,12) + 2 * level_module.level
+bow = random.randint(5,8) + 0.5 * level_module.level
+super_bow = random.randint(5,8) + 1 * level_module.level
+hyper_bow = random.randint(5,8) + 1,5 * level_module.level
+mega_bow = random.randint(5,8) + 2 * level_module.level
     
     
     
